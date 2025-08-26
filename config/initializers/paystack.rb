00196ac406ef6fcf473621f80a36af06ba17af41
fh <fhndiritu@gmail.com>
2025-08-26 17:01:16 +0300

    Configure paystack

--- config/initializers/paystack.rb
+++ config/initializers/paystack.rb
@@ -1,7 +1,7 @@
 PAYSTACK_CLIENT = Faraday.new(
-      url: ENV.fetch("PAYSTACK_BASE_URL"),
+      url: ENV.fetch("PAYSTACK_BASE_URL", nil),
       headers:  { "Content-Type" => "application/json" }
       ) do |conn|
-      conn.request :authorization, "Bearer", ENV.fetch("PAYSTACK_API_KEY")
+      conn.request :authorization, "Bearer", ENV.fetch("PAYSTACK_API_KEY", nil)
       conn.response :raise_error
     end
