6ecb83def5a5e4daf105bcb84018d6a244e7e16d
fh <fhndiritu@gmail.com>
2025-09-02 22:58:20 +0300

    Update cops

--- app/controllers/admin/landscapes_controller.rb
+++ app/controllers/admin/landscapes_controller.rb
@@ -4,7 +4,7 @@ module Admin
   class LandscapesController < Admin::BaseController
     def index
       if params[:day].present? && params[:day].to_i.positive?
-        @landscapes = Landscape.includes(:user).where(created_at: params[:day].to_i.days.ago.beginning_of_day..params[:day].to_i.days.ago.end_of_day).order(created_at: :desc)
+        @landscapes = Landscape.non_admin.includes(:user).where(created_at: params[:day].to_i.days.ago.beginning_of_day..params[:day].to_i.days.ago.end_of_day).order(created_at: :desc)
       else
         @landscapes = Landscape.non_admin.includes(:user).where(created_at: Time.zone.today.all_day).order(created_at: :desc)
       end
