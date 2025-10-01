d6429bd3207ba98a3df2979652fd7f0692b27166
fh <fhndiritu@gmail.com>
2025-10-01 21:11:12 +0300

    Reduce images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
-    @mask_requests = MaskRequest.complete.everyone.order(id: :desc).limit(20)
+    @mask_requests = MaskRequest.complete.everyone.order(id: :desc).limit(10)
   end
 
   def credits; end
