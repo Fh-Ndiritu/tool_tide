9c33d06a9466a29993d9d6b73376cd4138d8ad5c
fh <fhndiritu@gmail.com>
2025-10-17 17:32:22 +0300

    Show location requests with no tags

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,15 +2,20 @@ class LocationsController < ApplicationController
   skip_before_action :authenticate_user!
   def show
     @location = Location.find_by_slug(params[:slug])
-    @mask_requests = MaskRequest.unscoped
+   @mask_requests = MaskRequest.unscoped
     .complete
+    .left_outer_joins(:generation_taggings)
+    .where(generation_taggings: { tag_id: nil })
     .joins(canva: :user)
     .where(users: { admin: true })
     .references(canva: :user)
     .order("RANDOM()")
     .limit(4)
 
-    @text_requests = TextRequest.complete
+  # This finds all TextRequests that DO NOT have a matching record in generation_taggings
+  @text_requests = TextRequest.complete
+    .left_outer_joins(:generation_taggings)
+    .where(generation_taggings: { tag_id: nil })
     .joins(:user)
     .where(users: { admin: true })
     .references(:user)
