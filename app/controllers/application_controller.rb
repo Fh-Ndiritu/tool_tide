eafe95f435d6ecf20e66c3e5f4cd7ab26f1cb0bf
fh <fhndiritu@gmail.com>
2025-12-09 22:37:12 +0300

    Update Login pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,8 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :privacy_policy ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :privacy_policy, :user_name ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :user_name ])
   end
 
   def set_active_storage_url_options
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_08_230649) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_09_184020) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -315,6 +315,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_08_230649) do
     t.string "last_sign_in_ip"
     t.string "name"
     t.integer "source", default: 0
+    t.string "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
