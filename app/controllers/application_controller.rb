29bc29ee0d1919bc2640bb762c00a2b2f4a256ac
fh <fhndiritu@gmail.com>
2025-12-11 04:02:02 +0300

    Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   before_action :enforce_onboarding_flow, unless: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    if resource.is_a?(User) && resource.onboarding_stage != 'completed'
+    if resource.is_a?(User) && resource.onboarding_stage != "completed"
       new_canva_path
     else
       super
@@ -20,29 +20,29 @@ class ApplicationController < ActionController::Base
 
   def enforce_onboarding_flow
     return unless user_signed_in?
-    return if current_user.onboarding_stage == 'completed'
-    return unless ["canvas", "mask_requests"].include?(controller_name)
+    return if current_user.onboarding_stage == "completed"
+    return unless [ "canvas", "mask_requests" ].include?(controller_name)
 
     target_path = case current_user.onboarding_stage
-                  when 'fresh', 'welcome_seen'
+    when "fresh", "welcome_seen"
                     new_canva_path
-                  when 'image_uploaded'
+    when "image_uploaded"
                     latest_canva = current_user.canvas.last
                     latest_canva ? new_canva_mask_request_path(latest_canva) : new_canva_path
-                  when 'mask_drawn'
+    when "mask_drawn"
                     latest_mr = current_user.mask_requests.last
                     latest_mr ? edit_mask_request_path(latest_mr) : new_canva_path
-                  when 'style_selected'
+    when "style_selected"
                     latest_mr = current_user.mask_requests.last
                     latest_mr ? plants_mask_request_path(latest_mr) : new_canva_path
-                  when 'plants_viewed', 'first_result_viewed'
+    when "plants_viewed", "first_result_viewed"
                     latest_mr = current_user.mask_requests.last
                     latest_mr ? mask_request_path(latest_mr) : new_canva_path
-                  when 'text_editor_opened', 'refinement_generated'
+    when "text_editor_opened", "refinement_generated"
                     text_requests_path
-                  else
+    else
                     nil
-                  end
+    end
 
     if target_path && request.get? && request.path != target_path
       redirect_to target_path
