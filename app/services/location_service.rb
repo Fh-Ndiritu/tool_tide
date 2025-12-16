d4699f6fdbcddf8ca7e5ae2dcf075d24eca3e0a4
fh <fhndiritu@gmail.com>
2025-12-17 00:10:20 +0300

    Free Ip

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,5 +1,5 @@
-require 'net/http'
-require 'json'
+require "net/http"
+require "json"
 
 class LocationService
   LocationResult = Struct.new(:ip, :city, :region_name, :country_name, :country_code, :latitude, :longitude, :zip_code, :time_zone, keyword_init: true) do
@@ -17,12 +17,12 @@ class LocationService
     end
 
     def address
-      [city, region_name, country_name].compact.join(', ')
+      [ city, region_name, country_name ].compact.join(", ")
     end
   end
 
   def self.lookup(ip)
-    return nil if ip.blank? || ip == '127.0.0.1' || ip == '::1'
+    return nil if ip.blank? || ip == "127.0.0.1" || ip == "::1"
 
     begin
       uri = URI("https://free.freeipapi.com/api/json/#{ip}")
@@ -30,18 +30,18 @@ class LocationService
 
       if response.is_a?(Net::HTTPSuccess)
         data = JSON.parse(response.body)
-        return nil unless data['ipVersion'].present? # Check if we got a valid IP response
+        return nil unless data["ipVersion"].present? # Check if we got a valid IP response
 
         LocationResult.new(
-          ip: data['ipAddress'],
-          city: data['cityName'],
-          region_name: data['regionName'],
-          country_name: data['countryName'],
-          country_code: data['countryCode'],
-          latitude: data['latitude']&.to_f,
-          longitude: data['longitude']&.to_f,
-          zip_code: data['zipCode'],
-          time_zone: data['timeZone']
+          ip: data["ipAddress"],
+          city: data["cityName"],
+          region_name: data["regionName"],
+          country_name: data["countryName"],
+          country_code: data["countryCode"],
+          latitude: data["latitude"]&.to_f,
+          longitude: data["longitude"]&.to_f,
+          zip_code: data["zipCode"],
+          time_zone: data["timeZone"]
         )
       else
         Rails.logger.error("LocationService Error: #{response.code} - #{response.message}")
