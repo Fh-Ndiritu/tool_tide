15d377e7f364db164b45b32cd6d4e2f72e43661d
Fh Ndiritu <fhndiritu@gmail.com>
2026-01-12 17:37:09 +0300

    Remove old seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,36 +10,4 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-if Rails.env.development?
-  Tag.destroy_all
-  EVENTS.each do |name|
-    next if Tag.exists?(tag_class: :event, title: name)
-    tag = Tag.create!(tag_class: :event, title: name)
-    id = GenerationTagging.where(generation_type: 'TextRequest').pluck(:generation_id)
-    TextRequest.complete.where.not(id:).joins(:user).where(user: { admin: true }).limit(50).sample(2).each do |text_request|
-      next unless text_request.result_image.attached?
 
-      text_request.generation_taggings.create!(tag:)
-    end
-
-    MaskRequest.everyone.complete.limit(50).sample(2).each do |mask_request|
-      next unless mask_request.main_view.attached?
-      mask_request.generation_taggings.create!(tag:)
-    end
-  end
-
-  SEASONS.each do |name|
-    tag = Tag.create!(tag_class: :season, title: name)
-    TextRequest.complete.joins(:user).where(user: { admin: true }).limit(50).sample(2).each do |text_request|
-      next unless text_request.result_image.attached?
-
-      text_request.generation_taggings.create!(tag:)
-    end
-
-    MaskRequest.everyone.complete.limit(50).sample(2).each do |mask_request|
-      next unless mask_request.main_view.attached?
-      mask_request.generation_taggings.create!(tag:)
-    end
-  end
-
-end
