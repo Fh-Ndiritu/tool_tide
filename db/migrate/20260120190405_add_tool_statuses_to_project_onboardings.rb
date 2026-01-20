5bcc95ae1a17c3ec1d3a935bb879f598f398fe7a
Fh Ndiritu <fhndiritu@gmail.com>
2026-01-20 23:42:48 +0300

    Create onboarding tracker

--- db/migrate/20260120190405_add_tool_statuses_to_project_onboardings.rb
+++ db/migrate/20260120190405_add_tool_statuses_to_project_onboardings.rb
@@ -1,7 +1,13 @@
 class AddToolStatusesToProjectOnboardings < ActiveRecord::Migration[8.0]
   def change
-    add_column :project_onboardings, :style_presets_status, :integer
-    add_column :project_onboardings, :smart_fix_status, :integer
-    add_column :project_onboardings, :auto_fix_status, :integer
+    create_table :project_onboardings do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.integer :style_presets_status, default: 0, null: false
+      t.integer :smart_fix_status, default: 0, null: false
+      t.integer :auto_fix_status, default: 0, null: false
+
+      t.timestamps
+    end
   end
 end
