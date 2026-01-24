61f5cd207934354ea91e9806a41ee9b7faf74941
Fh Ndiritu <fhndiritu@gmail.com>
2026-01-24 10:01:30 +0300

    Move Legacy tools

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  skip_before_action :authenticate_user!, only: [:create, :new]
+  skip_before_action :redirect_to_canva, only: [:create, :new]
+
   def create
     super do |resource|
       cookies.permanent[:last_login_method] = "email"
+      resource.last_sign_in_device_type = browser.device.mobile? ? "mobile" : "desktop"
+      resource.save
     end
   end
 end
