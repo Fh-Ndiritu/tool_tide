355a8bb1636313b25bd9b6cf1ab96491a13e6e5c
Fh Ndiritu <fhndiritu@gmail.com>
2026-01-25 15:43:15 +0300

    Generate Designs from Layers

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -3,6 +3,32 @@ class DesignsController < ApplicationController
   before_action :set_project
 
   def create
+    if params[:project_layer_id].present?
+      source_layer = @project.project_layers.find(params[:project_layer_id])
+
+      title_source = source_layer.generation_type&.humanize || "Layer"
+      @design = @project.designs.create!(title: "Design from #{title_source}")
+
+      layer = @design.project_layers.new(
+          project: @project,
+          layer_type: :original,
+          progress: :complete
+      )
+
+      source_image_attachment = source_layer.display_image
+      if source_image_attachment&.attached?
+        layer.image.attach(source_image_attachment.blob)
+      end
+
+      if layer.save
+         @project.update(current_design: @design)
+         redirect_to project_path(@project, design_id: @design.id), notice: "New design created from layer!"
+      else
+         redirect_to project_path(@project), alert: "Failed to create design."
+      end
+      return
+    end
+
     image = params[:image]
 
     if image.blank?
@@ -12,7 +38,7 @@ class DesignsController < ApplicationController
 
     # Create the design (using filename as title, or a default)
     # The default title callbacks in Design model might handle blanks, but we want a useful title if possible.
-    design_title = image.original_filename.split('.').first.titleize
+    design_title = image.original_filename.split(".").first.titleize
 
     @design = @project.designs.create!(title: design_title)
 
