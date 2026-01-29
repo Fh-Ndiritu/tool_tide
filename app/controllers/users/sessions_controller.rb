438ba01ea9a1cb0b752ea418c85183f7a2e14f79
Fh Ndiritu <fhndiritu@gmail.com>
2026-01-30 00:49:57 +0300

    Temp

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  skip_before_action :authenticate_user!, only: [:create, :new]
-  skip_before_action :redirect_to_canva, only: [:create, :new]
+  skip_before_action :authenticate_user!, only: [ :create, :new ]
 
   def create
     super do |resource|
